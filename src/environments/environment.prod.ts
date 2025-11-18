7bbead0906d7940ea0ae0c79fc07d03779e49496
Jacopo Mattei <jacopo.mattei@team.blue>
2025-11-18 17:46:57 +0100

    o

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+// src/environments/environment.prod.ts
+export const environment = {
+  production: true,
+  // *** INSERISCI QUI L'URL PUBBLICO DEL BACKEND QUARKUS ***
+  apiUrl: 'https://brain-center-backend.onrender.com',
+  // Usa il tuo URL effettivo
+};
